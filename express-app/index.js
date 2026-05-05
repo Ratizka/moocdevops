aea2495f8bd1838a4f18bfb67a1d90fae993ab19
Ratizka <146937457+Ratizka@users.noreply.github.com>
2026-05-05 13:36:11 +0300

    paragraph

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! </h1>')
+  res.send('<h1>Hello World!</h1>')
+  res.send('<p> Testing </p>')
 })
 
 const PORT = 8080
